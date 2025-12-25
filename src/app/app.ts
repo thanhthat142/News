e31c44d84934f1c8214076df7f5b9f6fb418098c
thanhthat142 <wewebht@gmail.com>
2025-12-25 09:06:51 +0700

    learn angular

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('News');
+}
